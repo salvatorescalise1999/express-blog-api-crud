252958a66fd708855b7f9e754f9834e56e741d8c
Salvatore Scalise <salvatorescalise1999@gmail.com>
2026-02-09 15:47:48 +0100

    feat: aggiunta funzione show al postsController

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -4,6 +4,13 @@ function index(req, res) {
     res.send("Lista dei post");
 }
 
+function show(req, res) {
+  res.send(`Visualizzazione del post ${req.params.id}`);
+}
+
+
 module.exports = {
-    index
+    index, 
+    show,
+    
 };
