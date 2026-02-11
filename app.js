deebea41ad3c38f4788bb9ccedc231fb82f3965a
Salvatore Scalise <salvatorescalise1999@gmail.com>
2026-02-11 15:40:48 +0100

    feat: aggiunto middleware notFound per gestione rotte non registrate

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3000;
 // Importo router delle ricette
 const postsRouter = require('./routers/posts');
 
+// import del middelware di gestione di rotta inesistente
+const notFound = require("./middlewares/notFound");
+
 // Attivo cartella public per uso file statici
 app.use(express.static('public'));
 
@@ -19,6 +22,8 @@ app.get('/', (req, res) => {
 // Istanza delle rotte per risorsa ricette
 app.use("/posts", postsRouter)
 
+// registriamo middelware di gestione rotta inesistente
+app.use(notFound);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
